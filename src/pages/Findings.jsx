52f135cd1eeb50be0edc3b671e78f52effd74201
grant-franklin <grant.franklin@tcu.edu>
2026-04-21 11:10:01 -0500

    chore: Remove introductory text from Findings page

--- src/pages/Findings.jsx
+++ src/pages/Findings.jsx
@@ -5,14 +5,6 @@ import './Pages.css';
 export default function Findings() {
   return (
     <div className="page-container">
-      <h1>Findings</h1>
-      <section className="page-content">
-        <p>
-          This section presents the key findings from our research on NIL policies
-          and their impact on collegiate athletes.
-        </p>
-      </section>
-
       <Part4Dashboard />
     </div>
   );
